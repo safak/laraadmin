ad3c29d741db53b07449926b0ed00cb7899e5199
Safak <safak.kocaoglu@bil.omu.edu.tr>
2017-05-02 18:40:03 +0300

    uploading backend vendors

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,53 @@
+<footer>
+    <div class="pull-right">
+        Admin Theme Laravel Edition by <a href="https://safak.co">safak.co</a>
+    </div>
+    <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
+</div>
+</div>
+
+<!-- jQuery -->
+<script src="{{asset('laraadmin/vendors/jquery/dist/jquery.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset('laraadmin/vendors/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<!-- FastClick -->
+<script src="{{asset('laraadmin/vendors/fastclick/lib/fastclick.js')}}"></script>
+<!-- NProgress -->
+<script src="{{asset('laraadmin/vendors/nprogress/nprogress.js')}}"></script>
+<!-- Chart.js -->
+<script src="{{asset('laraadmin/vendors/Chart.js/dist/Chart.min.js')}}"></script>
+<!-- gauge.js -->
+<script src="{{asset('laraadmin/vendors/gauge.js/dist/gauge.min.js')}}"></script>
+<!-- bootstrap-progressbar -->
+<script src="{{asset('laraadmin/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js')}}"></script>
+<!-- iCheck -->
+<script src="{{asset('laraadmin/vendors/iCheck/icheck.min.js')}}"></script>
+<!-- Skycons -->
+<script src="{{asset('laraadmin/vendors/skycons/skycons.js')}}"></script>
+<!-- Flot -->
+<script src="{{asset('laraadmin/vendors/Flot/jquery.flot.js')}}"></script>
+<script src="{{asset('laraadmin/vendors/Flot/jquery.flot.pie.js')}}"></script>
+<script src="{{asset('laraadmin/vendors/Flot/jquery.flot.time.js')}}"></script>
+<script src="{{asset('laraadmin/vendors/Flot/jquery.flot.stack.js')}}"></script>
+<script src="{{asset('laraadmin/vendors/Flot/jquery.flot.resize.js')}}"></script>
+<!-- Flot plugins -->
+<script src="{{asset('laraadmin/vendors/flot.orderbars/js/jquery.flot.orderBars.js')}}"></script>
+<script src="{{asset('laraadmin/vendors/flot-spline/js/jquery.flot.spline.min.js')}}"></script>
+<script src="{{asset('laraadmin/vendors/flot.curvedlines/curvedLines.js')}}"></script>
+<!-- DateJS -->
+<script src="{{asset('laraadmin/vendors/DateJS/build/date.js')}}"></script>
+<!-- JQVMap -->
+<script src="{{asset('laraadmin/vendors/jqvmap/dist/jquery.vmap.js')}}"></script>
+<script src="{{asset('laraadmin/vendors/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
+<script src="{{asset('laraadmin/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js')}}"></script>
+<!-- bootstrap-daterangepicker -->
+<script src="{{asset('laraadmin/vendors/moment/min/moment.min.js')}}"></script>
+<script src="{{asset('laraadmin/vendors/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="{{asset('laraadmin/build/js/custom.min.js')}}"></script>
+
+</body>
+</html>
